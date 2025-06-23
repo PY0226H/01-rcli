1c2201c034586b5c995a814f546cfab1b44aa8c0
PY0226H <pyh20010226@gmail.com>
2025-06-23 13:38:43 -0700

    features: add tower-http support

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -6,6 +6,7 @@ use axum::{
     Router,
 };
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 #[derive(Debug)]
 struct HttpServeState {
@@ -14,11 +15,13 @@ struct HttpServeState {
 
 pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    info!("Serving {:?} on port {}", path, addr);
-    let state = HttpServeState { path };
-    //axum router
+    info!("Serving {:?} on {}", path, addr);
+
+    let state = HttpServeState { path: path.clone() };
+    // axum router
     let router = Router::new()
-        .route("/*path", get(file_handler))
+        .nest_service("/tower", ServeDir::new(path))
+        .route("/{*path}", get(file_handler))
         .with_state(Arc::new(state));
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
@@ -48,5 +51,19 @@ async fn file_handler(
                 )
             }
         }
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"));
     }
 }
